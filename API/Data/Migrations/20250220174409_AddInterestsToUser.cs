10a2e1752f326b04ce2bc0465e8f9b9b191ff58e
basel <Basel.Shabita@krammer-partner.de>
2025-02-21 09:31:39 +0100

    section 7

---
+++ API/Data/Migrations/20250220174409_AddInterestsToUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInterestsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookinFor",
+                table: "Users",
+                newName: "LookingFor");
+
+            migrationBuilder.RenameColumn(
+                name: "Intrests",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookingFor",
+                table: "Users",
+                newName: "LookinFor");
+
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Intrests");
+        }
+    }
+}
